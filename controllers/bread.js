3933814ac179bd257a867ab6577287093bb3c5da
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-08 13:14:15 -0400

    added index view

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
 
+// GET all bread
 router.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('index', { breads: Bread })
 })
 
+// GET bread by index
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.send(Bread[index])
